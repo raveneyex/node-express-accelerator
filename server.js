9ecb7e9c5135cc3e9dc28ce57117806809a7dc92
Andres Ossa <raveneyex@users.noreply.github.com>
2022-09-21 09:54:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, robot!');
+  res.send('Hello, r0bot!');
 });
 
 app.get('/health', (req, res) => {
